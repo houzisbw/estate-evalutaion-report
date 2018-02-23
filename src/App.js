b2ed37ba0fb936e88a0044cfe6211eae8145e77a
houzisbw <983134950@qq.com>
2018-02-23 15:12:47 +0800

    'eject配置文件以及添加scss支持'

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-import './App.css';
+import './index.scss';
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className="test">
             测试
       </div>
     );
