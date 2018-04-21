014065aa28b2fd5380f16f184eba373802937b06
houzisbw <983134950@qq.com>
2018-04-21 15:29:38 +0800

    派单地图显示标注功能完成

--- src/pages/HouseReviewArrange/HouseReviewArrange.js
+++ src/pages/HouseReviewArrange/HouseReviewArrange.js
@@ -8,11 +8,29 @@ import {connect} from 'react-redux'
 import {withRouter} from 'react-router-dom'
 import {Modal,Upload,Icon} from 'antd'
 class HouseReviewArrange extends React.Component{
-	constructor(props){
+	constructor(props) {
 		super(props);
 		this.state = {
 			//百度地图实例
-			map:null
+			map: null,
+			//搜索结果的坐标数组
+			locationList: [],
+			//地图标注的marker数组
+			markerList:[],
+			//地图标注label的样式
+			markerLabelStyle:{
+				color:'#fff',
+				backgroundColor:"#2aa056",
+				border:'none',
+				padding:'5px',
+				borderRadius:'3px',
+				boxShadow:'1px 2px 1px rgba(0,0,0,.15)'
+			},
+			//excel上传文件是否正在读取中
+			isExcelReading:false,
+			//读取失败的timerid
+			isReadingSuccessTimerId:null
+
 		}
 	}
 	componentDidMount(){
@@ -29,13 +47,39 @@ class HouseReviewArrange extends React.Component{
 			this.state.map.enableScrollWheelZoom(true);
 		})
 	}
+
+	//清除地图数据
+	clearMapData(){
+		this.setState({
+			markerList:[]
+		})
+	}
+
 	//excel上传触发的函数
 	handleExcelChange(obj){
+		//清除地图数据
+		this.clearMapData()
 		//获取input原生dom,不能通过obj参数获取
 		let input = this.refs.fileExcel;
 		if(!input.files) {
 			return;
 		}
+		//文件读取中
+		this.setState({
+			isExcelReading:true
+		});
+		//设置超时提示,10s后如果读取不出来提示失败
+		var tid = setTimeout(function(){
+			Modal.error({
+				title:'悲剧',
+				content:'由于未知原因文件读取失败，请重试'
+			})
+		},10*1000);
+		this.setState({
+			isReadingSuccessTimerId:tid
+		});
+
+
 		//获取上传的excel文件
 		var excel = input.files[0];
 		var reader = new FileReader();
@@ -43,19 +87,67 @@ class HouseReviewArrange extends React.Component{
 		//读取excel
 		reader.readAsBinaryString(excel);
 		//异步读取完成
+		var self = this;
 		reader.onload = function(e) {
 			var data = e.target.result;
 			wb = window.XLSX.read(data, {
 				type: 'binary'
 			});
 			var jsonData = window.XLSX.utils.sheet_to_json(wb.Sheets[wb.SheetNames[0]],{header:'A'});
+			//除去excel空白单元格
+			jsonData = jsonData.filter(function(item){
+				return item['A']
+			});
 			//置空value，使得二次上传相同文件有效，否则无法触发onchange事件
-			input.value = ''
+			input.value = '';
+			//搜索配置项
+			var tempLocationList = [];
+			var myGeo = new window.BMap.Geocoder();
+			//地址解析,用geoCoder，这里似乎并没有并发数量限制，注意如果某一行excel为空，则point也为空
+			var cnt=0;
+			var promiseList = []
+			for(let i=0;i<jsonData.length;i++){
+				var promise = new Promise(function(resolve,reject){
+					myGeo.getPoint(jsonData[i]['A'], function(point){
+							if (point) {
+								tempLocationList.push([point,jsonData[i]['A']]);
+								cnt++;
+							}
+							resolve()
+						},
+						"成都市");
+				});
+				promiseList.push(promise);
+			}
+			//等待所有异步请求都完成
+			var tempMarkerList = [];
+			Promise.all(promiseList).then((results)=>{
+				self.setState({
+					isExcelReading:false
+				});
+				clearTimeout(self.state.isReadingSuccessTimerId)
+				//在地图上标注
+				for(var i=0;i<tempLocationList.length;i++){
+					var point = tempLocationList[i][0];
+					var labelContent = tempLocationList[i][1];
+					var marker = new window.BMap.Marker(point);
+					var label = new window.BMap.Label(labelContent,{offset:new window.BMap.Size(20,-2)});
+					label.setStyle(self.state.markerLabelStyle)
+					marker.setLabel(label);
+					self.state.map.addOverlay(marker);
+					tempMarkerList.push(marker);
+				}
+
+				self.setState({
+					markerList:tempMarkerList
+				})
+			})
+
 		}
 
-
-
 	}
+
+
 	render(){
 		return (
 			//百度地图容器
@@ -72,9 +164,14 @@ class HouseReviewArrange extends React.Component{
 						</div>
 						<div className="right-size-map-operation-block-content">
 							<span className="fileinput-button">
-								<span><i className="fa fa-upload"></i> 上传</span>
+								{
+									this.state.isExcelReading?(
+										<span>读取中...</span>
+									):(<span><i className="fa fa-upload"></i> 上传</span>)
+								}
 								{/*添加ref属性方便获取input的files属性*/}
 								<input type="file"
+									   disabled={this.state.isExcelReading}
 									   ref = 'fileExcel'
 									   onChange={(e)=>{this.handleExcelChange(e)}}
 									   accept="application/vnd.ms-excel, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
